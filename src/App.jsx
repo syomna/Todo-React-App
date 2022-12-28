e6e4bcb1969c04b0588a8d3aef38756d0f4bd7bd
syomna <syomna444@gmail.com>
2022-12-28 20:49:02 +0200

    center-page

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,16 @@ function App() {
   });
   console.log(`todos ${todos}`)
   return (
-      <div className=" text-center mx-20">
+      <div className=' w-screen'>
+        <div className=" text-center w-80 m-auto">
       <h1 className=" text-amber-300">To-do App</h1>
-      <main className=" paper">
+      <main className=" paper ">
         <AddTodo />
         {todos.length > 0 ? <TodosList todos={todos}/> : <p className="my-20">No to-dos, start adding one!</p>}
         <Buttons />
       </main>
     </div>
+      </div>
   );
 }
 
